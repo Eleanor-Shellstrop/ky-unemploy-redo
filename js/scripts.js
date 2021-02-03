1d5c41b14cd29a9709bfb2095480cd5d06b4db4a
Anne Ensign <anne.ensign@gmail.com>
2021-02-03 16:05:32 -0500

    Link login.html to index.html, added JS scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 // HTML LINKS
-
+const login = document.getElementById("login");
 const calc = document.getElementById("calc");
 
 
-//FUNCTIONS
+/*====================================================================
+-----------------------------FUNCTIONS--------------------------------
+=====================================================================*/
 
+//Log in page to homepage 
+function logInButton() {
+    window.location.href = "../index.html"
+}
+
+//Home page to the calculator page
 function goToCalc() {
     window.location.href = "./html/calculator.html";
 }
